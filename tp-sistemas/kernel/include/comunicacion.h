ed2da92aae335decc7f13a582780bbad8f48450d
Ángel Zaracho <azaracho@frba.utn.edu.ar>
2025-05-03 20:08:44 -0300

    Update comunicacion.h

--- tp-sistemas/kernel/include/comunicacion.h
+++ tp-sistemas/kernel/include/comunicacion.h
@@ -1,1 +1,12 @@
+#ifndef COMUNICACION_H
+#define COMUNICACION_H
 
+// Comunicación con CPU
+void* atender_dispatch(void* args);
+void* atender_interrupt(void* args);
+
+// Funciones comunes de servidor
+int iniciar_servidor(char* puerto);
+int esperar_cliente(int socket_servidor);
+
+#endif // COMUNICACION_H
